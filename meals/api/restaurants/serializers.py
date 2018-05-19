ffba060e19939e1267d8643fc5dc45727084cd70
wangbo <wangbo@imusics.net>
2018-05-19 22:53:03 +0800

    fix

--- meals/api/restaurants/serializers.py
+++ meals/api/restaurants/serializers.py
@@ -6,7 +6,7 @@ class AddDishSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=30, allow_blank=False)
     price = serializers.FloatField(allow_null=False)
     restaurant_id = serializers.IntegerField(allow_null=False)
-    image_url = serializers.CharField(max_length=30, allow_null=False)
+    image_url = serializers.CharField(max_length=256, allow_null=False)
     support_times = serializers.CharField(max_length=128, allow_blank=False)
 
 
