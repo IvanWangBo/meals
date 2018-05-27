e093d193e1aaa7239b5e6cc19b726150bf22e9e3
wangbo <wangbo@imusics.net>
2018-05-27 15:59:01 +0800

    fix

--- meals/api/companies/views.py
+++ meals/api/companies/views.py
@@ -63,7 +63,7 @@ class AddCompanyView(HttpApiBaseView):
             admin_name = 'admin%03d' % company.id
             password = random.randint(10000000, 99999999)
             phone_number = data["phone_number"]
-            company_admin = cacher.create_user(admin_name, password, admin_type=UserAdminType.company, company_id=company.id, phone_number=phone_number)
+            company_admin = cacher.create_user(admin_name, password, admin_type=UserAdminType.company, company_id=company.id, phone_number=phone_number, real_name=u"管理员")
             company.save()
             return self.success_response({"company_id": company.id, "admin_name": admin_name, "password": password}, message=u'公司创建成功，管理员账号: %s, 初始密码: %s， 请牢记初始密码，或可在账号管理重置管理员密码。' % (admin_name, password))
         except Exception as err:
