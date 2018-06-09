bbc2029ef9d5820dd225b548da7b96b47d1a64b3
wangbo <wangbo@imusics.net>
2018-06-09 17:03:58 +0800

    -

--- meals/settings.py
+++ meals/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '22$xju^@uqj#!7h5(pghq!rrk#=-@#lj*w7-*@+8zf3d^xwt2b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
