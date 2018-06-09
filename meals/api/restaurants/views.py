5829dcad2e9194c81d303b30179d18f9a512bbec
wangbo <wangbo@imusics.net>
2018-06-09 16:15:21 +0800

    modify

--- meals/api/restaurants/views.py
+++ meals/api/restaurants/views.py
@@ -308,13 +308,21 @@ class RestaurantListView(HttpApiBaseView):
             is_order = request.GET.get('is_order', 0)
             all_restaurants = Restaurants.objects.filter(is_enabled=1)
             if is_order:
+                time_range_id = request.GET.get('time_range_id', 0)
+                dishes = Dishes.objects.filter(is_enabled=1)
+                flag_map = {}
+                for dish in dishes:
+                    support_times = json.loads(dish.support_times)
+                    for t in support_times:
+                        flag_map[dish.restaurant_id + t] = 1
                 company_id = self.get_login_user_company_id(request)
                 relations = RestaurantRelation.objects.filter(company_id=company_id, is_enabled=1)
                 restaurant_ids = [relation.restaurant_id for relation in relations]
                 restaurants = []
                 for restaurant in all_restaurants:
                     if restaurant.id in restaurant_ids:
-                        restaurants.append(restaurant)
+                        if flag_map.get(restaurant.id + time_range_id):
+                            restaurants.append(restaurant)
             else:
                 restaurants = all_restaurants
             results = [{
